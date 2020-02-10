6582c8e65ad03a609c5fe7605376a804cf5d5863
Mari <demarius.edwards@gmail.com>
2020-02-10 14:29:43 -0500

    2.10.20 update

--- lib/models/interface.rb
+++ lib/models/interface.rb
@@ -17,51 +17,71 @@ class Interface
     end
 
 
-  ########################## welcome ########################## 
+  ########################################### welcome #################################################### 
     def welcome
+      display_ye_world
       puts "Welcome to Ye World! Don't be alarmed if I speak in the third-person. That's the holy trinity!"
       sleep(0.5)
     end
   
-  ############################## Main Menu   ##################################
+  ########################################  Main Menu   ############################################################ 
     private
 
       
 
       def login_or_sign_up
-        puts "Enter ya name please"
-        answer = gets.chomp.downcase
-        @user = User.find_or_create_by(name: answer)
-        @user.points = 0
+        puts "First time, next or Last? Don't matter. Put ya name".colorize(:blue)
+        @user_input = gets.chomp.downcase
+        if user_valid
+          puts "WELL WELL, WELCOME BACK"
+          sleep 0.5
+          puts "You have #{@user.points} Ye points!"
+        else
+            @user = User.create(name: @user_input, points: 0)
+            puts "FIRST-TIMER! WELCOME!!"
+            @user.points = 0
+        end
+       
+      end
+
+      def user_valid
+        if User.find_by(name: @user_input)
+          @user = User.find_by(name: @user_input)
+          true
+        else
+          false
+        end
       end
 
       def main_menu
         pid = fork{exec 'afplay', "lib/music/Stronger_Kanye.mp3"}
         answer = prompt.select("What's good, #{@user.name}! How would you like to start?") do |menu|
-          menu.choice "Play Ye Trivia"
-          menu.choice "Make a Purchase"
-          menu.choice "View Ye Bank"
-          menu.choice "Update Name"
-          menu.choice "Leave Ye World"
+          menu.choice "Play Ye Trivia".colorize(:white)
+          menu.choice "Make a Purchase".colorize(:green)
+          menu.choice "View Ye Bank".colorize(:white)
+          menu.choice "Update Name".colorize(:green)
+          menu.choice "Leave Ye World".colorize(:red)
         end
-        if answer == "Play Ye Trivia"
+        if answer == "Play Ye Trivia".colorize(:white)
           system "killall afplay"
           # killall afplay
+
           puts "OK HERE WE GO!!"
           sleep(2)
+          system "clear"
           game_round(@user)
-        elsif answer == "Make a Purchase"
-          if @user.points = 0
-            puts "Play the game first, busta."
+        elsif answer == "Make a Purchase".colorize(:green)
+          if @user.points == 0
+            puts "Play the game first, no Yeebies."
             sleep(2)
             kill_music
             main_menu
           else
-            buying
+              buying
           end
-        elsif answer == "View Ye Bank"
-          if @user.points = 0
-            puts "Having money's not everything."
+        elsif answer == "View Ye Bank".colorize(:white)
+          if @user.points == 0
+            puts "Having money's not everything. You just broke homie."
             sleep(2)
             kill_music
             main_menu
@@ -69,23 +89,28 @@ class Interface
             puts "#{@user.points}"
             sleep(3)
             main_menu
+            kill_music
           end
-        elsif answer == "Update Name"
+        elsif answer == "Update Name".colorize(:green)
             edit_menu(@user)
-        elsif answer == "Leave Ye World"
+            kill_music
+        elsif answer == "Leave Ye World".colorize(:red)
+
           puts "Thanks for coming to YE WORLDDDDDDDD!"
-          sleep(3)
           kill_music
+          pid = fork{exec 'afplay', "lib/music/ye_haaa.mp3"}
+          sleep(3)
         end
 
       end
 
       def edit_menu(user)
-            answer = prompt.select("Edit name?") do |menu|
-              menu.choice "Ye"
-              menu.choice "Ne"
+            answer = prompt.select("____ my name") do |menu|
+              menu.choice "Change"
+              menu.choice "Delete"
+              menu.choice "Sike, do nada w/"
             end
-            if answer == "Ye"
+            if answer == "Change"
               puts "Put in ya new name"
               user_input = gets.chomp
               if user_input == User.all.select{|user_inst| user_inst.name}
@@ -94,12 +119,16 @@ class Interface
                 edit_menu(user)
               else
                 user.update(name: user_input)
+                kill_music
               end
-              kill_music
               main_menu
-            elsif answer == "Ne"
+            elsif answer == "Delete"
+              @user.destroy
               kill_music
+              run
+            elsif answer == "Sike, do nada w/"
               main_menu
+              kill_music
             end
 
       end
@@ -109,32 +138,46 @@ class Interface
         answer = prompt.select("What would you like to do next?") do |menu|
           menu.choice "Make a Purchase"
           menu.choice "View Ye Bank"
+          menu.choice "Back to Main Menu"
         end
         if answer == "Make a Purchase"
           buying
         elsif answer == "View Ye Bank"
           puts "#{@user.points}"
-          kill_music
           post_game_menu
+          kill_music
+        elsif answer == "Back to Main Menu"
+          kill_music
+          main_menu
         end
-        kill_music
       end
 
       def buying
-        stuff = Product.all.map(&:product_type)
-        things_2_buy = prompt.select("Even though you may or may not love Yeezy, YEEZY MERCH FOR EVERYBODY!", stuff)
-        puts "Have fun with your shit! Have wonderful Yeezy day!"
-        sleep(5)
-        kill_music
+        stuff = Product.all.map{|prod_inst| prod_inst.product_type}
+        things_2_buy = self.prompt.select("Even though you may or may not love Yeezy, YEEZY MERCH FOR EVERYBODY!".colorize(:green), stuff)
+        if things_2_buy == "Yeezy Shirt - 50"
+          picked_prod = Product.where(product_type: things_2_buy)
+          Purchase.create(user_id: @user.id, product_id: picked_prod[0].id, purchase?: true)
+          @user.points -= picked_prod[0].price
+          kill_music
+        elsif things_2_buy == "Yeezy Pants - 50"
+          picked_prod = Product.where(product_type: things_2_buy)
+          Purchase.create(user_id: @user.id, product_id: picked_prod[0].id, purchase?: true)
+          @user.points -= picked_prod[0].price
+          kill_music
+        elsif things_2_buy == "Yeezy Pop - free.99"
+          picked_prod = Product.where(product_type: things_2_buy)
+          Purchase.create(user_id: @user.id, product_id: picked_prod[0].id, purchase?: true)
+          @user.points -= picked_prod[0].price
+          kill_music
+        end
+        puts "Here you go! You now have #{@user.points} karma remaining Have a wonderful, Yeezy day!"
+        sleep(2.5)
         main_menu
         kill_music
       end
 
-      def login_or_sign_up
-        puts "Sign-up or log in"
-        answer = gets.chomp.downcase
-        @user = User.find_or_create_by(name: answer)
-      end
+      
 ####################################### Start of the game ############################################
       
       def game_round(user_instance)
@@ -152,7 +195,7 @@ class Interface
         system "clear"
         question_four
         @user.save
-        sleep(3)
+        sleep(3.5)
         system "clear"
         question_five
         @user.save
@@ -219,10 +262,12 @@ class Interface
         if answer == "Power"
           @user.points += 10
           puts "ok you got the second one. #{@user.points} points!"
+          kill_music
         else
           puts "get out"
+          kill_music
         end
-        kill_music
+        
         @user.points
         @user.save
       end
@@ -260,7 +305,7 @@ class Interface
           @user.points += 10
           puts "Ok, you know a lil sumthin. #{@user.points} points!"
         elsif answer == "Chicago"
-          puts "I actually didn't meet this girl until I was 3 years old"
+          puts "Wrong - I actually didn't meet this girl until I was 3 years old"
         elsif answer == "Kardashian Land"
           puts "*Ye stare*"
         else
@@ -328,7 +373,7 @@ class Interface
           puts "And I spent $400 buck on this!"
         elsif answer == "We don't knowwwww"
           @user.points -= 5 
-          puts "Oh you think you funny?? Minus -5 points! You now have #{@user.points}s"
+          puts "Oh you think you funny?? Minus -5 points! You now have #{@user.points} points"
         else
           puts "Wrong! But, go cop that new Yeezy!"
         end
@@ -406,13 +451,18 @@ class Interface
         elsif @user.points < 100
           puts "It's a shame you even took the test, but at least you got some right."
         else @user.points = 1
-          pid = fork{exec 'afplay', "lib/music/lollipop.mp3"}
+          pid = fork{exec 'afplay', "lib/music/oprah_ye.mp3"}
           puts "Pathetic, but luckily Yeezy has mercy on everyone. Take ya point and get you a Yeezy Pop!"
         end
       end
 
       def kill_music
         system "killall afplay"
+      end
+
+      def display_ye_world
+        title = TTY::Font.new(:doom)
+        puts title.write("WELCOME TO YE WORLD").colorize(:red)
       end
   end
 
